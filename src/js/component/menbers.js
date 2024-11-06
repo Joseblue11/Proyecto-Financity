6a441b80f8b4f8e7585508e28ff59658cea99e19
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2024-11-06 22:28:47 +0000

    cambio de avatares por imagenes

--- src/js/component/menbers.js
+++ src/js/component/menbers.js
@@ -6,112 +6,74 @@ export const Members = () => {
     <div className="Members" id="Ciudadanos">
       <h2 className="Tittle-Members">Secretarios Distritales</h2>
       <div className="Content">
-        {/* <div className="Members-part1">
-          <img
-            src="https://static.vecteezy.com/system/resources/previews/021/682/355/non_2x/avatar-with-a-young-face-pictures-of-men-and-women-of-various-nationalities-vector.jpg"
-            className="members-image1 img-fluid pt-2"
-            alt="..."
-          />
-          <div>
-            <h3 className="Members-title1">Alcalde</h3>
-            <p className="Members-text1">Don Cashlo</p>
-          </div>
-        </div> */}
         <div className="Members-part2">
-          {/* <div>
-            <h3 className="Members-title1">Secretarios Distritales</h3>
-          </div> */}
-        
           <div className="Group-members">
             <div className="members">
               <img
-                src="https://static.vecteezy.com/system/resources/previews/021/682/318/non_2x/avatar-with-a-young-face-pictures-of-men-and-women-of-various-nationalities-vector.jpg"
+                src="https://res.cloudinary.com/dmyppqapj/image/upload/v1730931562/IMG-20241106-WA0076_nsz1iv.jpg"
                 className="members-image img-fluid pt-2"
                 alt="..."
               />
               <div className="card-menber">
                 <h3 className="Members-title">Jesús González</h3>
-                <p className="Members-text">Secretarío Distrital<br />Capital</p>
+                <p className="Members-text">Secretario Distrital<br />Capital</p>
               </div>
             </div>
             <div className="members">
               <img
-                   src="https://cdn.vectorstock.com/i/preview-1x/67/57/avatar-with-a-young-face-pictures-of-men-vector-46356757.webp"
+                   src="https://res.cloudinary.com/dmyppqapj/image/upload/v1730931562/IMG-20241106-WA0074_cn8d2r.jpg"
                 className="members-image img-fluid pt-2"
                 alt="..."
               />
               <div className="card-menber">
                 <h3 className="Members-title">Jailene Tirado</h3>
-                <p className="Members-text">Secretaría Distrital<br />Gastos</p>
+                <p className="Members-text">Secretaria Distrital<br />Gastos</p>
               </div>
             </div>
             <div className="members">
               <img
-                src="https://static.vecteezy.com/system/resources/previews/021/682/326/non_2x/avatar-with-a-young-face-pictures-of-men-and-women-of-various-nationalities-vector.jpg"
+                src="https://res.cloudinary.com/dmyppqapj/image/upload/v1730931562/IMG-20241106-WA0073_x7gjbh.jpg"
                 className="members-image img-fluid pt-2"
                 alt="..."
               />
               <div className="card-menber">
                 <h3 className="Members-title">Gabriel Heredia</h3>
-                <p className="Members-text">Secretarío Distrital<br />Ahorro</p>
+                <p className="Members-text">Secretario Distrital<br />Ahorro</p>
               </div>
             </div>
             <div className="members">
               <img
-                src="https://cdn.vectorstock.com/i/preview-1x/67/56/avatar-with-a-young-face-pictures-of-men-vector-46356756.webp"
+                src="https://res.cloudinary.com/dmyppqapj/image/upload/v1730931562/IMG-20241106-WA0078_fzb3mk.jpg"
                 className="members-image img-fluid pt-2"
                 alt="..."
               />
               <div className="card-menber">
                 <h3 className="Members-title">Yuri Ramírez</h3>
-                <p className="Members-text">Secretaría Distrital<br />Inversion</p>
+                <p className="Members-text">Secretaria Distrital<br />Inversion</p>
               </div>
             </div>
             <div className="members">
               <img
-                src="https://static.vecteezy.com/system/resources/previews/021/682/331/non_2x/avatar-with-a-young-face-pictures-of-men-and-women-of-various-nationalities-vector.jpg"
+                src="https://res.cloudinary.com/dmyppqapj/image/upload/v1730931562/IMG-20241106-WA0075_mp1gll.jpg"
                 className="members-image img-fluid pt-2"
                 alt="..."
               />
               <div className="card-menber">
                 <h3 className="Members-title">Valeria Palma</h3>
-                <p className="Members-text">Secretaría Distrital<br />Plan de Ahorro</p>
+                <p className="Members-text">Secretaria Distrital<br />Plan de Ahorro</p>
               </div>
             </div>
             <div className="members">
               <img
-                src="https://cdn.vectorstock.com/i/preview-1x/67/45/avatar-with-a-young-face-pictures-of-men-vector-46356745.webp"
+                src="https://res.cloudinary.com/dmyppqapj/image/upload/v1730931562/IMG-20241106-WA0077_eqlvjt.jpg"
                 className="members-image img-fluid pt-2"
                 alt="..."
               />
               <div className="card-menber">
                 <h3 className="Members-title">Jonayker de la Rosa</h3>
-                <p className="Members-text">Secretarío Distrital<br />Presupuesto</p>
+                <p className="Members-text">Secretario Distrital<br />Presupuesto</p>
               </div>
-            </div>
-            {/* <div className="members">
-              <img
-                src="https://cdn.vectorstock.com/i/preview-1x/67/55/avatar-with-a-young-face-pictures-of-men-vector-46356755.webp"
-                className="members-image img-fluid pt-2"
-                alt="..."
-              />
-              <div className="card-menber">
-                <h3 className="Members-title">Eifrancis Sanz</h3>
-                <p className="Members-text">Diseñadora Grafica</p>
-              </div>
-            </div>
-            <div className="members">
-              <img
-                src="https://cdn.vectorstock.com/i/preview-1x/67/50/avatar-with-a-young-face-pictures-of-men-vector-46356750.webp"
-                className="members-image img-fluid pt-2"
-                alt="..."
-              />
-              <div className="card-menber">
-                <h3 className="Members-title">José Tovar</h3>
-                <p className="Members-text">Desarrollador Web</p>
-              </div>
-            </div> */}
-            
+            </div>      
           </div>
         </div>
       </div>
